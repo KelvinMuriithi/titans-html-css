a00dfd3e564b49d2b202e12cb5bbf6bf615d1de8
Kelvinm <kelvinmurithi18@gmail.com>
2025-02-27 10:03:47 +0300

    added javascript expressions and variables

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const studentName = "Paul Simiyu"; //constant expression
+console.log(studentName)
+
+var x = 5; //assignment expression
+console.log(x)
+
+let y = 15;
+
+console.log(y)
+
+let age= y // variable lookup expression
+console.log(age)
